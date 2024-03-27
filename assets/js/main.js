88df60981d4dc25d273550d8a9618ce9bc55cf1d
Ziad Adel <ziadadel00120@gmail.com>
2024-03-27 02:29:29 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,19 +203,6 @@
   /**
    * Initiate portfolio lightbox 
    */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Initiate portfolio details lightbox 
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
-
   /**
    * Portfolio details slider
    */
